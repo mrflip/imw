f9ccb1262cce0d4b99714789543786e368238c85
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-06-19 12:15:33 -0400

    Created config.rb to handle reading configuration files.

---
+++ lib/imw/utils/config.rb
@@ -0,0 +1,17 @@
+#
+# h2. lib/imw/utils/config.rb -- configuration parsing
+#
+# == About
+#
+# This file contains classes for parsing IMW configuration files.
+#
+# Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
+# Copyright:: Copyright (c) 2008 infochimps.org
+# License::   GPL 3.0
+# Website::   http://infinitemonkeywrench.org/
+# 
+
+
+
+
+# puts "#{File.basename(__FILE__)}: Something clever" # at bottom
