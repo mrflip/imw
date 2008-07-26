9830b213dd614d29fe51b0f853f4ad53bd2879da
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-07-26 16:07:38 -0400

    Rewrote archive.rb so that it knows no details about particular archiving programs but implements instead skeletal methods to build/use archives of any type.  Including classes will have an instance attribute `archive' which lets them customize the behavior of the IMW::Archive module for their particular needs.

--- lib/imw/model/files/archive.rb
+++ lib/imw/model/files/archive.rb
@@ -1,16 +1,33 @@
 #
-# h2. lib/imw/model/files/archive.rb -- classes for manipulating various archive formats
+# h2. lib/imw/model/files/archive.rb -- describes archives of files
 #
 # == About
 #
-# Module used for making an object act like an archive of files.  The
-# including object must define a class variable +archiving+ which must
-# be a hash with the following keys:
+# Module for describing known archive types.  An including archive
+# type's class must define an instance variable +archive+ which is a
+# hash with the following required keys:
 #
-# <tt>:type</tt> (required):: a Symbol defining a type of archive (likely one of of <tt>:tar</tt>, <tt>:rar</tt>, or <tt>:zip</tt>.
-# <tt>:program</tt>:: a String defining the program to be used.  If none is provided, the program is chosen from IMW::EXTERNAL_PROGRAMS according to the <tt>:type</tt> of the archive
-# <tt>:create_flags</tt> (required):: a String containing the flags to use when creating an archive
-# <tt>:extract_flags</tt> (required):: a String containing the flags to use when extracting an archive or a Symbol naming an instance method which takes a directory as an argument and returns a String containing the flags
+# <tt>:program</tt>:: a symbol naming the program to be used.  It
+# should match one of the symbols in <tt>IMW::EXTERNAL_PROGRAMS</tt>
+# 
+# <tt>:create_flags</tt>:: a string of flags to pass to the archiving
+# program when creating the archive
+# 
+# <tt>:append_flags</tt>:: a string of flags to pass to the archiving
+# program when appending files to the archive
+# 
+# <tt>:extract_flags</tt>:: a string of flags to pass to the archiving
+# program when extracting the archive
+# 
+# <tt>:list_flags</tt>:: a string of flags to pass to the archiving
+# program when listing the archive's contents
+#
+# THe +archive+ hash may also contain the entry:
+#
+# <tt>:unarchiving_program</tt>:: a symbol naming the program to be
+# used to list/extract the archive.  Useful only if this program
+# differs from the program used to create the archive in the first
+# place (i.e. - zip & unzip).
 #
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
@@ -18,48 +35,77 @@
 # Website::   http://infinitemonkeywrench.org/
 # 
 
-require 'rubygems'
-require 'activesupport'
+require 'fileutils'
 
 require 'imw/utils'
-require 'imw/utils/core_extensions'
 
 module IMW
 
-  module Archive
+  module Files
 
-    # Include files into this archive.  If the archive is empty then
-    # create it first.
-    #
-    # Options:
-    # <tt>:del_orig_files</tt> (false):: delete original copies of files after storing them in this archive
-    def self.create files, opts = {}
-      opts.reverse_merge!({:del_orig_files => false})
-      paths = files.map { |file| file.class == String ? File.expand_path(file) : File.expand_path(file.path) }
-      program = self.archiving[:program] || IMW::EXTERNAL_PROGRAMS[self.archiving[:type]]
-      flags = self.archiving[:create_flags]
-      command =  program + ' ' + flags + ' ' + paths
-      IMW.system(command)
-      if opts[:del_orig_files] then files.each {|file| FileUtils.rm(file)} end
-    end
+    module Archive
 
-    # Extract files from this archive.
-    # 
-    # Options:
-    # <tt>:del_archive</tt> (false):: delete the archive after extracting files
-    def self.extract directory = nil, opts = {}
-      opts.reverse_merge!({:del_archive => false})
-      program = self.archiving[:program] || IMW::EXTERNAL_PROGRAMS[self.archiving[:type]]
-      flags = self.archiving[:extract_flags]
-      if flags.class == Symbol then flags = self.send(flags,directory) end # customized flags on a per extraction basis
-      command =  program + ' ' + flags + ' ' + paths
-      IMW.system(command)
-      if opts[:del_archive] then FileUtils.rm(self.path) end
+      attr_reader :archive
+
+      # Create this archive containing the given +files+.
+      #
+      # Options:
+      # <tt>:force</tt> (false):: overwrite any existing archive at this path.
+      def create files, opts = {}
+        opts.reverse_merge!({:force => false})
+        raise IMW::Error.new("An archive already exists at #{@path}.") if exist? and not opts[:force]
+
+        files = [files] if files.class == String
+
+        FileUtils.cd(@dirname)
+        command = ([IMW::EXTERNAL_PROGRAMS[@archive[:program]],@archive[:create_flags],@basename] + files).join ' '
+        IMW.system(command)
+      end
+
+      # Append to this archive the given +files+.
+      def append files
+        files = [files] if files.class == String
+        
+        FileUtils.cd(@dirname)
+        command = ([IMW::EXTERNAL_PROGRAMS[@archive[:program]],@archive[:append_flags],@basename] + files).join ' '
+        IMW.system(command)
+      end
+
+      # Extract the files from this archive to its directory.
+      def extract
+        FileUtils.cd(@dirname)
+
+        program = @archive[:unarchiving_program] || @archive[:program]
+        command = [IMW::EXTERNAL_PROGRAMS[program],@archive[:extract_flags],@basename].join ' '
+        IMW.system(command)
+      end
+
+      # Return a (sorted) list of contents in this archive.
+      def contents
+        FileUtils.cd(@dirname)
+
+        program = @archive[:unarchiving_program] || @archive[:program]        
+        command = [IMW::EXTERNAL_PROGRAMS[program],@archive[:list_flags],@basename].join ' '
+        # run the command first through `IMW.system' so we can ensure
+        # it exited cleanly before we try to capture its output
+        IMW.system(command)
+        output = `#{command}`
+        archive_contents_string_to_array(output)
+      end
+
+      # Parse and format the output from the archive program's "list"
+      # command into an array of filenames.
+      #
+      # An including class can customize this method to match the
+      # output from the archiving program of that class.
+      def archive_contents_string_to_array string
+        string.split("\n").sort
+      end
+      
     end
     
   end
-
+    
 end
 
-
-# puts "#{File.basename(__FILE__)}: Something clever" # at bottom
+# puts "#{File.basename(__FILE__)}: Put it all in one place so that when something goes wrong you'll know it immediately.  You'll regret it, but at least you'll know." # at bottom
