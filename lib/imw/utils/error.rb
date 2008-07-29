a39947cb8b8256f0cad83b16e53deee770ed5a22
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-07-29 02:22:20 -0400

    Added a few errors and fixed a bug where IMW::PathError wasn't inheriting from anything...

--- lib/imw/utils/error.rb
+++ lib/imw/utils/error.rb
@@ -3,7 +3,7 @@
 #
 # == About
 #
-# Error objects for IMW>
+# Error objects for IMW.
 #
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
@@ -18,9 +18,15 @@ module IMW
   class Error < StandardError
   end
 
+  class TypeError < TypeError
+  end
+
+  class ArgumentError < ArgumentError
+  end
+
   # An error meant to be used when a system call goes awry.  It will
   # report exit status and the process id of the offending call.
-  class SystemCallError < RuntimeError
+  class SystemCallError < IMW::Error
 
     def initialize(message)
       @message = message
@@ -37,7 +43,7 @@ module IMW
   end
 
   # A error for improperly specified, inappropriate, or broken paths.
-  class PathError
+  class PathError < IMW::Error
   end
-  
+
 end
