97b2e581123ceba852be094b562162d6ea8d12c8
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-07-30 12:24:59 -0400

    Cleaned up the IMW::Directory class a little, and decided that we're probably not going to use it.  Not for a while.

--- lib/imw/model/directory.rb
+++ lib/imw/model/directory.rb
@@ -3,9 +3,14 @@
 #
 # == About
 #
-# This file implements a class for parsing strings into directories
-# and instructions for accessing those directories, whether they be
-# local or remote over some protocol.
+# Defines a <tt>IMW::Directory</tt> class which abstracts the
+# differences between local and remote directories and adds some
+# convenience methods.
+#
+# There's nothing here yet but a non-functioning version of a local
+# directory.  For now it's probably best to pretend that this class
+# doesn't actually exist.  We'll write it and give it real
+# functionality if we need to.
 #
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
@@ -13,14 +18,18 @@
 # Website::   http://infinitemonkeywrench.org/
 # 
 
-require 'imw/utils/config'
+require 'imw/utils'
 
 module IMW
 
   class Directory
 
-    attr_reader :path
+    # Methods of directory access that have been implemented.
+    ACCESS_METHODS = [:local]
 
+    attr_reader :path, :method
+
+    private
     def initialize handle
       parse_handle(handle)
     end
@@ -28,23 +37,15 @@ module IMW
     # Parse the string `handle' for special prefixes and path
     # resolution.
     def parse_handle(handle)
-      if handle =~ /^~/ then
-        @path = File.expand_path(handle)
-        @method = :local_disk
-      elsif handle =~ /\// then
-        @path = handle
-        @method = :local_disk
-      elsif handle =~ /^[a-zA-Z]+:/ then
-        raise NotImplementedError.new("Directories with prefixes like `ssh:' or `ftp:' are not currently implemented in IMW.  Sorry!")
+      if /^[a-zA-Z]+:/.match handle then
+        raise IMW::NotImplementedError.new("Directories with prefixes like `ssh:' or `ftp:' are not currently implemented in IMW.  Sorry!")
       else
-        @path = [IMW::Config::Directories[:imw_root],handle].join('/')
-        @method = :local_disk
+        @path = File.expand_path(handle)
+        @method = :local
       end
     end
     
   end
-
 end
-
 
 # puts "#{File.basename(__FILE__)}: You idly wonder where you could find a listing of reliable wrench polishers." # at bottom
