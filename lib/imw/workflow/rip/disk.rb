fcb79b95a04595be566e1cbc36344b7794b65868
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-07-31 11:52:17 -0400

    Began rewriting the rip from local disk stuff but it's not done; going to merge flip's changes to mainline now and just wanted to get the working tree clean.

--- lib/imw/workflow/rip/disk.rb
+++ lib/imw/workflow/rip/disk.rb
@@ -1,10 +1,10 @@
 #
-# h2. lib/imw/workflow/rip/disk.rb -- obtaining data from local/remote disks
+# h2. lib/imw/workflow/rip/disk.rb -- ripping data from local disk
 #
 # == About
 #
-# This file implements several functions for reading data from local
-# and remote filesystems into the IMW system.
+# Contains methods for ripping data from the local disk to the
+# appropriate source directory.
 #
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
@@ -19,15 +19,13 @@ module IMW
   module Workflow
     module Rip
 
-      # Copies data from a local disk into the directory for this
-      # source.
-      #
-      # +local_data+ is a string or array of strings containing
-      # absolute paths to files or directories that should be copied.
-      # Consult <tt>FileUtils.smart_copy</tt> for useful options as
-      # keyword options passed to this method will be passed down to
-      # the FileUtils methods it calls.
-      def rip_from_local_disk(local_data,opts={})
+      # Copies data from a local disk into the appropriate directory
+      # for +source+.
+      def from_local_disk_to source
+        
+
+
+        
         @source = "local_disk"
         FileUtils.mkdir_p(self.path_to(:ripd)) if not File.exist?(self.path_to(:ripd))
         FileUtils.smart_copy(local_data,self.path_to(:ripd),opts)
