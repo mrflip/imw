4349119f634f759a39c42a5c9abc74f0652ebc62
Philip (flip) Kromer <flip@infochimps.org>
2008-08-01 05:39:38 -0500

    Added as_dset convenience function: call 'as_dset __FILE__' and it will assemble the standard arsenal of paths, optionally scaffolding if you like

---
+++ lib/imw/dataset/loaddump.rb
@@ -0,0 +1,59 @@
+require 'YAML'
+require 'imw/utils/paths'
+
+module IMW
+  class DataSet
+    #
+    # Guess the file format of a given file
+    #
+    def self.file_format filename
+      File.extname(path_to(filename)).gsub(/^\./,'').to_sym
+    end
+
+    #
+    # KLUDGE -- the below should sometime evolve into
+    # a facade + factory.  This worksfornow.
+    #
+
+    #
+    # Return a new dataset from the given file
+    #
+    #
+    #
+    def self.load filename, &block
+      announce "Loading #{filename}"
+      format ||= file_format(filename)
+      file     = File.open(path_to(filename))
+      data = case format
+      when :yaml
+        YAML.load file
+      else
+        raise "Don't know how to dump a #{format} file"
+      end
+      data = yield data if block
+      self.new(data)
+    end
+
+    #
+    # Dump the given data structure to a file,
+    # guessing the format and all that stupid crap.
+    #
+    def self.dump data, filename, format = nil
+      announce "Dumping #{filename}"
+      format ||= DataSet.file_format(filename)
+      file     = File.open(path_to(filename), 'w')
+      case format
+      when :yaml
+        YAML.dump(data, file)
+      else
+        raise "Don't know how to dump a #{format} file"
+      end
+    end
+    #
+    # dispatch to DataSet.dump
+    def dump *args
+      self.class.dump self, *args
+    end
+
+  end
+end
--- lib/imw/dataset/stats.rb
+++ lib/imw/dataset/stats.rb
@@ -7,28 +7,51 @@ module IMW
     # Runs down one column/attribute of a dataset
     # returning counts for that column
     #
-    def hist field
+    def hist slicer
       counts = { }
-      self.data.each do |el|
-        counts[el[field]] ||= 0
-        counts[el[field]]  += 1
+      els = slice(slicer)
+      els.each do |el|
+        counts[el] ||= 0
+        counts[el]  += 1
       end
-      counts.map{ |el,ct| [ct,el] }
+      self.class.new(counts.map{ |el,ct| [ct,el] })
+    end
+
+    def slice slicer
+      case
+      when slicer.respond_to?(:call) then self.map{ |row| slicer.call(row) }
+      else
+        self.map{ |row| row[slicer] }
+      end
     end
 
     #
     # Report
     #
-    def report field, opts={}
-      opts = { :n_top => 20 }.merge opts
-      # Histogram
-      counts = hist field
-      top = counts.sort[-opts[:n_top]..-1]
-      puts "Top #{opts[:n_top]} elements for field #{field}:"
+    def report slicer, opts={}
+      opts.reverse_merge! :n_top => 20, :hist_args => [], :fmt => "%7d\t%s", :do_hist => true
+      counts = hist(slicer)
+      report_hist  data, counts, slicer, opts if opts[:do_hist]
+      report_sizes data, counts, slicer, opts
+    end
+
+    def report_sizes data, counts, slicer, opts={}
+      fmt  = opts[:fmt]
+      puts fmt % [counts.length,              "unique elements"]
+      puts fmt % [data.length,                "total elements"]
+      puts fmt % [counts.find_all(&:nil?).length, "nil elements"]
+      uniqvals = counts.map{|ct,el| el}.reject(&:nil?)
+      puts " min:\t#{uniqvals.min}"
+      puts " max:\t#{uniqvals.max}"
+    end
+
+    # Most popular
+    def report_hist data, counts, slicer, opts={}
+      top = counts.sort_by{|ct,el| ct}[-opts[:n_top]..-1]
+      puts "Top #{opts[:n_top]} elements for slice through #{slicer}:"
       puts " -freq-\t-element-"
-      puts top.map{ |el,ct| "%7d\t%s" % [el,ct] }
-      # Number of elements
-      puts " total:\t#{data.length} elements)"
+      puts top.map{ |ct,el| opts[:fmt] % [ct,el] }
+      puts "-------\t-------"
     end
 
   end
