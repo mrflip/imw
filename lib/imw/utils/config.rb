4c0ad8fc157d17b593b5d8e7d5ef78ec0ae12b9d
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-08-03 18:33:57 -0400

    Rewrote config.rb to have a Config module which loads the various configuration files with the correct precedence.

--- lib/imw/utils/config.rb
+++ lib/imw/utils/config.rb
@@ -3,14 +3,36 @@
 #
 # == About
 #
-# This file defines constants that describe the external utilties IMW
-# depends upon, the directories it is allowed to write in, the file
-# extensions it knows about, &c.
+# IMW looks for configuration settings in the following places, in
+# order of increasing precedence:
 #
-# Right now, this information is simply coded as Ruby structures.
-# Eventually, this will be replaced by a module which parses YAML
-# configuration files to construct the same objects.  As IMW is in
-# flux, hard-coding in the constants is a little easier for now.
+#   1. Settings defined in this file as Ruby objects.
+#
+#   2. From the <tt>etc/imwrc</tt> file in the IMW root directory.
+#
+#   3. From the <tt>.imwrc</tt> file in the user's home directory (the
+#      filename can be changed; see
+#      <tt>IMW::Config::USER_CONFIG_FILE_BASENAME</tt>).
+#
+#   4. From the file defined by the environment variable +IMWRC+ (the
+#      value can be changed; see
+#      <tt>IMW::Config::USER_CONFIG_FILE_ENV_VARIABLE</tt>
+#
+# Settings not found in one configuration location will be searched
+# for in locations of lesser precedence.
+#
+# *Note:* configuration files are _currently_ meant to by plain Ruby code that
+# will be directly executed.  Eventually, they are meant to be
+# replaced by YAML files that will be parsed by IMW.
+#
+# Relevant settings include
+#
+#   * interfaces with external programs (+tar+, +wget+, &c.)
+#   * paths to directories where IMW reads/writes files
+#
+# For more detailed information, read the code of this file or read
+# the example configuration file provided at <tt>etc/imwrc</tt>.
+#
 #
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
@@ -22,90 +44,109 @@ require 'rubygems'
 require 'yaml'
 
 module IMW
-  attr_reader :cfg
 
-  #
-  def self.imw_root_dir
-    File.join(File.dirname(__FILE__), '../../..')
-  end
+  module Config
 
-  # User configuration filename
-  def self.config_file
-    ENV['IMWRC'] || File.join(ENV['HOME'], '.imwrc')
-  end
+    ################################################################
+    ## Find all the external configuration settings
+    ################################################################
+    private
+    # Returns the root of the IMW source base.
+    def self.imw_root
+      File.join(File.dirname(__FILE__), '../../..')
+    end
 
-  # Default configuration filename
-  def self.config_base
-    File.join(imw_root_dir, 'etc', 'imwrc.rb')
-  end
+    # Returns the default configuration filename, <tt>etc/imwrc</tt>
+    # in the IMW root directory.
+    def self.default_config_filename
+      File.join(imw_root, 'etc', 'imwrc.rb')
+    end
 
-  # Source the config files
-  def self.load_config
-    require config_base
-    require config_file if File.exist? config_file
-  end
+    # we require the default configuration file
+    eval(File.open(default_config_filename).read) if File.exist? default_config_filename
 
+    # The basename of the file looked for in the user's home directory
+    # for configuration settings.
+    USER_CONFIG_FILE_BASENAME = ".imwrc" unless defined? USER_CONFIG_FILE_BASENAME
 
-  # Paths to external programs.
-  EXTERNAL_PROGRAMS = {
-    :tar => "tar",
-    :rar => "rar",
-    :zip => "zip",
-    :unzip => "unzip",
-    :gzip => "gzip",
-    :bzip2 => "bzip2",
-    :wget => "wget"
-  } unless defined? EXTERNAL_PROGRAMS
+    # Returns the path to the configuration file in the home directory
+    # of each user.
+    def self.user_home_config_filename
+      File.join(ENV['HOME'], USER_CONFIG_FILE_BASENAME)
+    end
 
-  # Directories where IMW will write and look for files.
-  DIRECTORIES = {
-    :root => "/home/dhruv/projects/infochimps/imw",
+    # we now require the configuration file in the home directory of
+    # the user
+    eval(File.open(user_home_config_filename).read) if File.exist? user_home_config_filename
 
-    :pool => "/home/dhruv/projects/infochimps/pool",
-    :sources => "/home/dhruv/projects/infochimps/pool/sources",
-    :datasets => "/home/dhruv/projects/infochimps/pool/datasets",
+    # The environment variable checked for a file with configuration
+    # settings.
+    USER_CONFIG_FILE_ENV_VARIABLE = "IMWRC" unless defined? USER_CONFIG_FILE_ENV_VARIABLE
+
+    # we now require the configuration file pointed at by the user's
+    # environment variable
+    eval(File.open(USER_CONFIG_FILE_ENV_VARIABLE).read) if File.exist? USER_CONFIG_FILE_ENV_VARIABLE
+
+    ################################################################
+    ## Now make settings here if they're not defined already in the above
+    ################################################################
     
-    :data => "/home/dhruv/projects/infochimps/data",
-    :ripd => "/home/dhruv/projects/infochimps/data/ripd",
-    :xtrd => "/home/dhruv/projects/infochimps/data/xtrd",
-    :prsd => "/home/dhruv/projects/infochimps/data/prsd",
-    :mungd => "/home/dhruv/projects/infochimps/data/mungd",
-    :fixd => "/home/dhruv/projects/infochimps/data/fixd",
-    :pkgd => "/home/dhruv/projects/infochimps/data/pkgd",
+    # Paths to external programs.
+    IMW::EXTERNAL_PROGRAMS = {
+      :tar => "tar",
+      :rar => "rar",
+      :zip => "zip",
+      :unzip => "unzip",
+      :gzip => "gzip",
+      :bzip2 => "bzip2",
+      :wget => "wget"
+    } unless defined? IMW::EXTERNAL_PROGRAMS
 
-    :dump => "/tmp/imw"
-  } unless defined? DIRECTORIES
+    # Directories where IMW will write and look for files.
+    IMW::DIRECTORIES = {
+      :sources => File.expand_path("~/imw/pool/sources"),
+      :datasets => File.expand_path("~/imw/pool/datasets"),
+      
+      :data => File.expand_path("~/imw/data"),
+      :ripd => File.expand_path("~/imw/data/ripd"),
+      :xtrd => File.expand_path("~/imw/data/xtrd"),
+      :prsd => File.expand_path("~/imw/data/prsd"),
+      :mungd => File.expand_path("~/imw/data/mungd"),
+      :fixd => File.expand_path("~/imw/data/fixd"),
+      :pkgd => File.expand_path("~/imw/data/pkgd"),
+
+      :dump => "/tmp/imw"
+    } unless defined? IMW::DIRECTORIES
 
 
-  module Files
-    # Correspondence between extensions and file types.  Used by
-    # <tt>IMW::Files.identify</tt> to identify files based on known
-    # extensions.
-    #
-    # File type strings should be stripped of the leading
-    # <tt>IMW::Files</tt> prefix, i.e. - the file object
-    # <tt>IMW::Files::Bz2</tt> should be referenced by the string
-    # <tt>"Bz2"</tt>.
-    EXTENSIONS = {
-      ".bz2" => "Bz2",
-      ".gz" => "Gz",
-      ".tar.bz2" => "TarBz2",
-      ".tbz2" => "TarBz2",
-      ".tar.gz" => "TarGz",
-      ".tgz" => "TarGz",
-      ".rar" => "Rar",
-      ".zip" => "Zip",
-      ".txt" => "Text",
-      ".ascii" => "Text",
-      ".csv" => "Csv",
-      ".xml" => "Xml",
-      ".html" => "Html",
-      ".yaml" => "Yaml",
-      ".yml" => "Yaml"
-    } unless defined? EXTENSIONS
+    module Files
+      # Correspondence between extensions and file types.  Used by
+      # <tt>IMW::Files.identify</tt> to identify files based on known
+      # extensions.
+      #
+      # File type strings should be stripped of the leading
+      # <tt>IMW::Files</tt> prefix, i.e. - the file object
+      # <tt>IMW::Files::Bz2</tt> should be referenced by the string
+      # <tt>"Bz2"</tt>.
+      EXTENSIONS = {
+        ".bz2" => "Bz2",
+        ".gz" => "Gz",
+        ".tar.bz2" => "TarBz2",
+        ".tbz2" => "TarBz2",
+        ".tar.gz" => "TarGz",
+        ".tgz" => "TarGz",
+        ".rar" => "Rar",
+        ".zip" => "Zip",
+        ".txt" => "Text",
+        ".ascii" => "Text",
+        ".csv" => "Csv",
+        ".xml" => "Xml",
+        ".html" => "Html",
+        ".yaml" => "Yaml",
+        ".yml" => "Yaml"
+      } unless defined? IMW::Files::EXTENSIONS
+    end
   end
 end
-
-IMW.load_config
 
 # puts "#{File.basename(__FILE__)}: You carefully adjust the settings on your Monkeywrench.  Beware, glob-monsters!" # at bottom
