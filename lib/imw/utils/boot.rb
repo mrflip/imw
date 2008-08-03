97a78b8dcb5c5e20600763505c7fcf1042e7233c
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-08-03 18:34:28 -0400

    Quantities which should be set on the initial load of an IMW process should now be set in imw/utils/boot.rb.

--- lib/imw/utils/boot.rb
+++ lib/imw/utils/boot.rb
@@ -12,14 +12,20 @@
 # Website::   http://infinitemonkeywrench.org/
 #
 
-require 'yaml'
+require 'fileutils'
 
-# Checks that dependencies on Ruby gems and external programs are
-# satisfied
-#
-# FIXME needs to be written!
-def check_dependencies()
-  true
+require 'imw/utils'
+require 'imw/model/pool'
+require 'imw/utils/paths'
+
+module IMW
+
+  # A pool for all the sources at this IMW installation.
+  IMW::SOURCES = IMW::Pool.new(IMW::DIRECTORIES[:sources])
+
+  # Initialize IMW.paths to hold various useful paths at startup.
+  IMW::DIRECTORIES.each_pair {|name,path| IMW.add_path name, path }
+  
 end
 
 # puts "#{File.basename(__FILE__)}: You heft up your Infinite Monkeywrench for the first time and marvel at how something so powerful could be made so wondrous light!" # at bottom
