d1a24aa411d1c422d01c3e7d3128ff04537c9f8e
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-08-03 14:20:12 -0400

    Restructured the comments for clarity.

--- lib/imw/utils/paths.rb
+++ lib/imw/utils/paths.rb
@@ -3,11 +3,14 @@
 #
 # == About
 #
-# IMW uses lots of different directories to keep information on data
-# and datasets separate.  This module interfaces with the
-# configuration files to establish the paths to these IMW directories
-# and provides functions and mixins for IMW objects to use to access
-# these paths.
+# IMW keeps files for sources and datasets are in many directories on
+# the system.  IMW objects come with +path_to+ methods which point at
+# relevant directories.
+#
+# The functions defined here are intended for quick and simple access
+# to directories, providing a simple shorthand mechanism for
+# identifying and recalling paths, suitable for use in interactive
+# sessions or in simple scripts.
 #
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
@@ -15,32 +18,26 @@
 # Website::   http://infinitemonkeywrench.org/
 #
 
-require 'imw/utils'
-
 module IMW
 
-  module Paths
-    # Returns the root of workflow `step'
-    def self.root_of(step)
-      unless IMW::DIRECTORIES.has_key? step
-        raise IMW::ArgumentError.new("No such IMW directory, `#{step}'.  Choose from #{IMW::DIRECTORIES.keys.map do |key| '`' + key.to_s + '\'' end.join ', '}")
-      end
-      IMW::DIRECTORIES[step]
-    end
-  end
-
-  # expands a shorthand workflow path specification to an
-  # actual file path.
+  public
+  # Returns pathnames from shorthand input in various formats.
   #
-  # Ex:
+  # It does nothing to strings but maintains a hash between symbols
+  # and paths (accessible via <tt>IMW.add_path</tt>,
+  # <tt>IMW.rm_path</tt>, &c.) so that symbols can be used as
+  # shorthand for actual paths.
   #
-  # Dir[IMW.path_to(:temp, 'foo', '*')]
+  # Quick file globbing:
   #
-  # IMW.add_path :mlb_08, 'gd2.mlb.com/components/game/mlb/year_2008'
-  # IMW.path_to :ripd, :mlb_08, 'month_06', 'day_08', 'miniscoreboard.xml'
-  # => (...)/data/ripd/gd2.mlb.com/components/game/mlb/year_2008/month_06/day_08/miniscoreboard.xml
+  #   Dir.glob(IMW.path_to(:tmp, 'foo', '*'))
   #
-  def path_to *dir_specs
+  # Adding a one-off directory for a dataset:
+  #
+  #   IMW.add_path :mlb_08, 'gd2.mlb.com/components/game/mlb/year_2008'
+  #   IMW.path_to :ripd, :mlb_08, 'month_06', 'day_08', 'miniscoreboard.xml'
+  #     => (...)/data/ripd/gd2.mlb.com/components/game/mlb/year_2008/month_06/day_08/miniscoreboard.xml
+  def self.path_to *dir_specs
     # recursively expand
     expanded = dir_specs.flatten.map do |dir_spec|
       dir_spec.is_a?(Symbol) ? path_to(paths[dir_spec]) : dir_spec
@@ -51,13 +48,18 @@ module IMW
     joined
   end
 
-  #
-  # Adds a symbolic path for expansion by path_to
-  #
-  def add_path sym, *dirs
+  # Adds the symbolic path +sym+ defined by +dirs+.
+  def self.add_path sym, *dirs
     @@paths[sym] = dirs.flatten
   end
-  def paths() @@paths  end
+
+  # Removes the symbolic path +sym+.
+  def self.delete_path sym
+    @@paths.delete sym
+  end
+  
+  # Return the list of paths.
+  def self.paths() @@paths  end
 
 end
 
