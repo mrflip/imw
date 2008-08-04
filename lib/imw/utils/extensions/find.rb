ead836c112a520641c1f490f6f928fcdd4af7aa9
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-08-04 01:22:12 -0400

    Added ability to find files in directory (abslute or relative) with including/excluding regexp search in Find extensions.

--- lib/imw/utils/extensions/find.rb
+++ lib/imw/utils/extensions/find.rb
@@ -13,13 +13,22 @@
 # 
 
 require 'find'
+require 'imw/utils/extensions/hash'
 
 module Find
 
   # Returns a list of paths relative to +directory+ of the files (and
-  # only the files) it contains, optionally matching the given
-  # +regex+.
-  def self.files_relative_to_directory directory, regex = //
+  # only the files) it contains.
+  #
+  # Options include:
+  #
+  # <tt>:include</tt>:: a regular expression that the relative part of
+  #                     each path must match.
+  #
+  # <tt>:exclude</tt>:: a regular expression that the relative part of
+  #                     each path must not match.
+  def self.files_relative_to_directory directory, opts = {}
+    opts.reverse_merge!({:include => nil, :exclude => nil})
     directory = File.expand_path directory
     files = []
     Find.find(directory) do |path|
@@ -27,21 +36,50 @@ module Find
         abs_path = File.expand_path(path)
         # the +1 is there because we want a relative path
         rel_path = abs_path.slice(directory.length + 1,abs_path.length)
-        files << rel_path if regex.match rel_path
+        # FIXME this is a stupid way of testing both regexen
+        if opts[:include] then
+          matches = opts[:include].match(rel_path)
+        elsif opts[:exclude] then
+          matches = opts[:exclude].match(rel_path)
+        elsif opts[:include] && opts[:exclude] then
+          matches = opts[:include].match(rel_path) && opts[:exclude].match(rel_path)
+        else
+          matches = true
+        end
+        files << rel_path if matches
       end
+      files
     end
     files
   end
 
   # Returns a list of absolute paths in +directory+ of the files (and
-  # only the files) it contains, optionally matching the given
-  # +regex+.
-  def self.files_in_directory directory, regex = //
+  # only the files) it contains.
+  # 
+  # Options include:
+  #
+  # <tt>:include</tt>:: a regular expression that each path must
+  #                     match.
+  #
+  # <tt>:exclude</tt>:: a regular expression that each path must not
+  #                     match.
+  def self.files_in_directory directory, opts = {}
+    opts.reverse_merge!({:include => nil, :exclude => nil})
     directory = File.expand_path directory
     files = []
     Find.find(directory) do |path|
       unless File.directory?(path)
-        files << path if regex.match path
+        # FIXME this is a stupid way of testing both regexen
+        if opts[:include] then
+          matches = opts[:include].match(path)
+        elsif opts[:exclude] then
+          matches = opts[:exclude].match(path)
+        elsif opts[:include] && opts[:exclude] then
+          matches = opts[:include].match(path) && opts[:exclude].match(path)
+        else
+          matches = true
+        end
+        files << path if matches
       end
     end
     files
