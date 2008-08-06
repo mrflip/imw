5e402b6d1e25e877b1cc9874ceb0294f5d02f875
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-08-06 12:41:08 -0400

    Got rid of silly configuration options in imw.rb and made it require boot.rb.

--- lib/imw.rb
+++ lib/imw.rb
@@ -3,24 +3,16 @@
 #
 # == About
 #
-# This file includes the rest of the IMW code base and contains some
-# "global" constants.
-#
+# This file is the entry-point to the IMW library.  It starts up by
+# loading up all the other required files, including those responsible
+# for parsing configuration settings and setting initial constants.
+# 
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
 # License::   GPL 3.0
 # Website::   http://infinitemonkeywrench.org/
-# 
+#
 
-module IMW
+require 'imw/utils/boot'
 
-  # Standard aliases for 'yes'.
-  Yeses = ['Yes','YES','yes','Y','y']
-  # Standard aliases for 'no'.
-  Nos = ['No','NO','no','N','n']
-  # Standard format string for timestamps.
-  TimestampFormat = "%Y-%m-%d-%H-%M-%S"
-
-end
-
-# puts "#{File.basename(__FILE__)}: Behold your Infinite Monkeywrench and regard it with awe as it has overwhelmed mightier men than ye." # at bottom
+# puts "#{File.basename(__FILE__)}: Behold, the weighty, the munificent, the Infinite Monkeywrench! Approach it with care: it has overwhelmed mightier monkeys than ye." # at bottom
