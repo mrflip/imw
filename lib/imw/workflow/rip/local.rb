1e9db2af17959a1320096837f04358a661af4d63
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-08-08 17:04:30 -0400

    Scrubbed the IMW::Workflow::Rip hierarchy and instead want to keep things simpler: IMW::Rip and so on.  Also rewrote the (awful) "smart_copy" function I had living in FileUtils extensions in lib/imw/workflow/rip/local.rb where it simply copies files from multiple paths to a target directory with a flat hierarchy, though it accepts a block to modify this behavior.

--- lib/imw/workflow/rip/local.rb
+++ lib/imw/workflow/rip/local.rb
@@ -3,8 +3,8 @@
 #
 # == About
 #
-# Contains methods for ripping data from the local disk to the
-# appropriate source directory.
+# Contains methods for ripping data from various paths on the local
+# disk to a target directory.
 #
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
@@ -18,64 +18,70 @@ require 'imw/utils'
 require 'imw/utils/extensions/find'
 
 module IMW
-  module Workflow
-    module Rip
+  module Rip
 
-      # Copies data from +paths+ to the <tt>:ripd</tt> directory for
-      # this source, setting the +source+ attribute to "local".
-      #
-      # +paths+ can be a collection of files or directories (which
-      # will be recursively copied).
-      #
-      # If called without a block, files will be copied from their
-      # source directory to this source's <tt>:ripd</tt> directory and
-      # named with their basenames, without any further hierarchical
-      # directory structure:
-      #
-      #   source.rip_from_paths("/path/to/first/file.txt", "/different/path/to/second/thing.dat")
-      #
-      # will result in the files <tt>file.txt</tt> and
-      # <tt>thing.dat</tt> in this source's <tt>:ripd</tt>.
-      #
-      # If called with a block, then given the path of the original
-      # file, the block must return a string representing the path of
-      # the copy, relative to the <tt>:ripd</tt> directory of this
-      # source.  If the block returns +nil+, then the file will not be
-      # copied:
-      #
-      #   source.rip_from_paths("/path/to/file.txt", "/path/to/second/thing.dat", "/path/to/third.html") do |file|
-      #     case File.extname file
-      #     when ".txt"
-      #       File.join("txt",file)
-      #     when ".dat"
-      #       File.join("dat",file)
-      #     else
-      #       nil
-      #     end
-      #   end
-      #
-      # will result in the files <tt>txt/file.txt</tt> and
-      # <tt>dat/thing.dat</tt> in the <tt>:ripd</tt> directory for
-      # this source.
-      #
-      # Whether or not a block is given, files will not be
-      # overwritten; the filenames will be made unique by appending a
-      # numeric suffix a la +wget+, i.e. - <tt>common_filename.txt</tt> would
-      # become <tt>common_filename.txt.1</tt> and so on (see
-      # <tt>File.uniquify</tt>).
-      def self.rip_from_paths *paths
-        files = paths.flatten.map {|path| Find.files_in_directory(path) }.flatten
-        @source = "local"
-        files.each do |file|
-          # use the basename of the file unless given a block
-          filename = File.join(path_to(:ripd), (block_given? ? yield(file) : File.basename(file)))
-          filename or next
-          File.uniquify filename # ensure we don't clobber existing files
-          FileUtils.cp file, filename
-        end
+    # Copies data from +paths+ to the +target_directory+.
+    #
+    # +paths+ can be a collection of files or directories (which
+    # will be recursively copied).
+    #
+    # If called without a block, files will be copied to the
+    # +target_directory+ and named with their basenames, without any
+    # further hierarchical directory structure:
+    #
+    #   copy_files_to_target("/target/directory","/path/to/first/file.txt", "/different/path/to/second/thing.dat")
+    #
+    # will result in the files <tt>/target/directory/file.txt</tt>
+    # and <tt>/target/directorything.dat</tt>.
+    #
+    # If called with a block, then given the path of the original
+    # file, the block must return a string representing the path of
+    # the copy, relative to the +target_directory+.  If the block
+    # returns +nil+, then the file will not be copied:
+    #
+    #   copy_files_to_target("/target/directory", "/path/to/file.txt", "/path/to/second/thing.dat", "/path/to/third.html") do |file|
+    #     case File.extname file
+    #     when ".txt"
+    #       File.join("txt",File.basename file) # put text files in /target/directory/txt
+    #     when ".dat"
+    #       File.join("dat",File.basename file) # put dat files in /target/directory/dat
+    #     else
+    #       nil # don't copy any other kinds of files
+    #     end
+    #   end
+    #
+    # will result in the files
+    # <tt>/target/directory/txt/file.txt</tt> and
+    # <tt>/target/directory/dat/thing.dat</tt>.
+    #
+    # Whether or not a block is given, files will not be
+    # overwritten; the filenames will be made unique by appending a
+    # numeric suffix a la +wget+, i.e. -
+    # <tt>common_filename.txt</tt> would become
+    # <tt>common_filename.txt.1</tt> or
+    # <tt>common_filename.txt.2</tt> and so on until a non-existing
+    # filename was found(see <tt>File.uniquify</tt>).
+    def self.copy_files_to_target target_directory, *paths
+      target_directory = File.expand_path(target_directory)
+      raise IMW::PathError.new("#{target_directory} is not a valid directory") unless File.directory?(target_directory)
+      files = paths.flatten.map do |path|
+        raise IMW::PathError.new("#{path} does not exist") unless File.exist? path
+        File.directory?(path) ? Find.files_in_directory(path) : path
       end
-
+      files.flatten.each do |file|
+        # use the basename of the file unless given a block
+        basename = (block_given? ? yield(file) : File.basename(file))
+        basename or next # if the block returns nil then don't copy this file
+        
+        filename = File.join(target_directory, basename)
+        File.uniquify filename # ensure we don't clobber existing files
+        
+        FileUtils.mkdir_p(File.dirname(filename)) unless File.exist?(File.dirname(filename)) # ensure the directory will exist
+        
+        FileUtils.cp file, filename
+      end
     end
+
   end
 end
 
---
+++ spec/imw/workflow/rip/local_spec.rb
@@ -0,0 +1,88 @@
+#
+# h2. spec/imw/workflow/rip/local_spec.rb -- specs for copying files from local disk
+#
+# == About
+#
+# Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
+# Copyright:: Copyright (c) 2008 infochimps.org
+# License::   GPL 3.0
+# Website::   http://infinitemonkeywrench.org/
+# 
+require File.join(File.dirname(__FILE__),'../../../spec_helper')
+require IMW_SPEC_DIR + "/imw/matchers/without_regard_to_order_matcher.rb"
+
+require 'fileutils'
+
+require 'imw/utils/random'
+require 'imw/utils/extensions/find'
+require 'imw/workflow/rip/local'
+
+describe "Ripping from local disk" do
+
+  include Spec::Matchers::IMW
+
+  before(:all) do
+    @root_directory = IMW::DIRECTORIES[:dump] + "/local_spec"
+    @file1 = @root_directory + "/first.csv"
+    
+    @source_directory1 = @root_directory + "/source1"
+    @file2 = @source_directory1 + "/second.txt"
+    @file3 = @source_directory1 + "/third.csv"
+
+    @source_directory2 = @root_directory + "/source2"
+    @file4 = @source_directory2 + "/fourth.txt"
+    @file5a = @source_directory2 + "/fifth-shared.yaml"
+
+    @source_directory3 = @source_directory2 + "/source3-nested"
+    @file5b = @source_directory3 + "/fifth-shared.yaml"
+    
+    @target_directory = @root_directory + "/target"
+  end
+
+  before(:each) do
+    FileUtils.mkdir([@root_directory,@source_directory1,@source_directory2,@source_directory3,@target_directory])
+    [@file1,@file2,@file3,@file4,@file5a,@file5b].each {|file| IMW::Random.file(file)}
+  end
+
+  after(:each) do
+    FileUtils.rm_rf @root_directory
+  end
+
+  def basenames_of files
+    files.map {|file| File.basename file}
+  end
+  
+  it "should raise an error when attempting to copy to a non-existent target directory" do
+    FileUtils.rm_rf @target_directory
+    lambda { IMW::Rip.from_local_disk(@target_directory,@source_directory1)}.should raise_error(IMW::PathError)
+  end
+
+  it "should copy all files in all directories and paths recursively to the target directory without any hierarchy" do
+    IMW::Rip.from_local_disk(@target_directory,@file1,@source_directory1,@source_directory2)
+    Find.files_relative_to_directory(@target_directory).should match_without_regard_to_order(basenames_of([@file1,@file2,@file3,@file4,@file5a]))
+  end
+
+  it "should accept a block which establishes a hierarchy to be created in the target directory and which skips copying certain files if it returns nil" do
+
+    # complicated block to copy files to sub-directories of the target
+    # directory depending on their extension
+    IMW::Rip.from_local_disk(@target_directory,@file1,@source_directory1,@source_directory2) do |path|
+      if File.extname(path) == '.txt' then
+        File.join('txt',File.basename(path)) # put text files in txt
+      elsif File.extname(path) == '.csv' then
+        File.join("csv",File.basename(path)) # put csv files in csv
+      else
+        nil # don't copy other extensions
+      end
+    end
+
+    # what we would expect to see from that block
+    txt = [@file2,@file4].map {|path| File.join("txt",File.basename(path))}
+    csv = [@file1,@file3].map {|path| File.join("csv",File.basename(path))}
+
+    Find.files_relative_to_directory(@target_directory).should match_without_regard_to_order(txt + csv)
+  end
+      
+end
+
+# puts "#{File.basename(__FILE__)}: Having found the platter you were looking for, you stare at it, examining your reflection.  What a handsome chimp you are!" # at bottom
