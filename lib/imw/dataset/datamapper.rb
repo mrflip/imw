e8bd8d19fc1fa5f98dc35cfaea2453449241dc70
Philip (flip) Kromer <flip@infochimps.org>
2008-08-11 07:08:42 -0500

    A little bit less impolite on the DB connection setup -- now uses .imwrc.rb

--- lib/imw/dataset/datamapper.rb
+++ lib/imw/dataset/datamapper.rb
@@ -1,13 +1,19 @@
 require 'rubygems'
-# gem     'dm-core'
 require 'dm-core'
+require 'dm-ar-finders'
+require 'dm-aggregates'
 
 module IMW
   class DataSet
-    # A MySQL 4.x+ connection:
-    def self.setup_connection protocol, user, pass, host, dbname
-      DataMapper.setup(:default, "%s://%s:%s@%s/%s" %[protocol, user, pass, host, dbname])
+    # Connect to a remote database
+    def self.setup_remote_connection options
+      params = options.values_at(:protocol, :username, :password, :hostname, :dbname)
+      DataMapper.setup(:default, "%s://%s:%s@%s/%s" % params)
     end
-
+    # Connect to a local database
+    def self.setup_local_connection options
+      params = options.values_at(:protocol, :dbpath, :dbname)
+      DataMapper.setup(:default, "%s://%s/%s" % options)
+    end
   end
 end
