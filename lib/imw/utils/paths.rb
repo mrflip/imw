8539b06e64e59f0757302de84ae8ce3188e36eaa
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-09-06 02:16:47 -0500

    Merged some conflicts.

--- lib/imw/utils/paths.rb
+++ lib/imw/utils/paths.rb
@@ -3,14 +3,11 @@
 #
 # == About
 #
-# IMW keeps files for sources and datasets are in many directories on
-# the system.  IMW objects come with +path_to+ methods which point at
-# relevant directories.
-#
-# The functions defined here are intended for quick and simple access
-# to directories, providing a simple shorthand mechanism for
-# identifying and recalling paths, suitable for use in interactive
-# sessions or in simple scripts.
+# IMW uses lots of different directories to keep information on data
+# and datasets separate.  This module interfaces with the
+# configuration files to establish the paths to these IMW directories
+# and provides functions and mixins for IMW objects to use to access
+# these paths.
 #
 # Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
 # Copyright:: Copyright (c) 2008 infochimps.org
@@ -18,56 +15,105 @@
 # Website::   http://infinitemonkeywrench.org/
 #
 
+require 'imw/utils'
+
 module IMW
 
-  @@paths = {}
-
-  public
-  # Returns pathnames from shorthand input in various formats.
-  #
-  # It does nothing to strings but maintains a hash between symbols
-  # and paths (accessible via <tt>IMW.add_path</tt>,
-  # <tt>IMW.rm_path</tt>, &c.) so that symbols can be used as
-  # shorthand for actual paths.
-  #
-  # Quick file globbing:
-  #
-  #   Dir.glob(IMW.path_to(:tmp, 'foo', '*'))
-  #
-  # Adding a one-off directory for a dataset:
-  #
-  #   IMW.add_path :mlb_08, 'gd2.mlb.com/components/game/mlb/year_2008'
-  #   IMW.path_to :ripd, :mlb_08, 'month_06', 'day_08', 'miniscoreboard.xml'
-  #     => (...)/data/ripd/gd2.mlb.com/components/game/mlb/year_2008/month_06/day_08/miniscoreboard.xml
-  def self.path_to *dir_specs
-    # recursively expand
-    expanded = dir_specs.flatten.map do |dir_spec|
-      dir_spec.is_a?(Symbol) ? path_to(paths[dir_spec]) : dir_spec
+  module Paths
+    # Returns the root of workflow `step'
+    def self.root_of(step)
+      unless IMW::DIRECTORIES.has_key? step
+        raise IMW::ArgumentError.new("No such IMW directory, `#{step}'.  Choose from #{IMW::DIRECTORIES.keys.map do |key| '`' + key.to_s + '\'' end.join ', '}")
+      end
+      IMW::DIRECTORIES[step]
     end
-    joined = File.join(*expanded)
-    # memoize
-    # @@paths[dir_specs[0]] = joined if (dir_specs.length==1 && dir_specs[0].is_a?(Symbol))
+  end
+
+  # expands a shorthand workflow path specification to an
+  # actual file path.
+  #
+  # Ex:
+  #
+  # Dir[IMW.path_to(:temp, 'foo', '*')]
+  #
+  # IMW.add_path :mlb_08, 'gd2.mlb.com/components/game/mlb/year_2008'
+  # IMW.path_to :ripd, :mlb_08, 'month_06', 'day_08', 'miniscoreboard.xml'
+  # => (...)/data/ripd/gd2.mlb.com/components/game/mlb/year_2008/month_06/day_08/miniscoreboard.xml
+  #
+  def path_to *pathsegs
+    path = Pathname.new path_to_helper(*pathsegs)
+    path.absolute? ? File.expand_path(path) : path
+  end
+
+  def path_to_helper *pathsegs
+    # recursively expand
+    expanded = pathsegs.flatten.compact.map do |pathseg|
+      pathseg.is_a?(Symbol) ? path_to(paths[pathseg]) : pathseg
+    end
+    begin joined = File.join(*expanded) rescue raise("Can't find path to '#{pathsegs}' from #{joined.inspect}"); end
     joined
   end
 
-  # Adds the symbolic path +sym+ defined by +dirs+.
-  def self.add_path sym, *dirs
-    
-@@paths[sym] = dirs.flatten
+  #
+  # Adds a symbolic path for expansion by path_to
+  #
+  def add_path sym, *pathsegs
+    @@paths[sym] = pathsegs.flatten
+  end
+  def paths() @@paths  end
 
 
-
-
-
+  #
+  # Makes a set of symbolic paths referenced to this dataset's path
+  #
+  def as_dset_paths dset_path, cut_dirs
+    if dset_path.is_a?(String)
+      require 'pathname'
+      dset_path = Pathname.new(dset_path).realpath.to_s
+      dset_path = dset_path.chomp('/').split('/')[-(cut_dirs+2)..-cut_dirs]
+    end
+    add_path :dset, dset_path
+    add_path :me,   [:scripts_root, :dset]
+    [:rawd, :temp, :fixd, :log, :ripd].each do |seg|
+      add_path seg, [:me, seg.to_s]
+    end
   end
 
-  # Removes the symbolic path +sym+.
-  def self.delete_path sym
-    @@paths.delete sym
+  def as_dset dset_path, opts={}
+    opts = { :cut_dirs => 2, :scaffold => false }.merge opts
+    as_dset_paths dset_path, opts[:cut_dirs]
+    if opts[:scaffold]
+      require 'imw/workflow/scaffold'
+      scaffold_dset
+    end
   end
-  
-  # Return the list of paths.
-  def self.paths() @@paths  end
+
+  #
+  # Canonical log file
+  #
+  # a file in your
+  #
+  def log_file_name *args
+    log_head = @@paths.include?(:log) ? :log : [:log_root, 'meta']
+    log_name = [args, path_datecode].flatten.join('-') + '.log'
+    log_path = path_to(log_head, log_name)
+    # user can add paths, so re-take the dirname
+    mkdir_p File.dirname(log_path)
+    log_path
+  end
+
+
+  def path_datecode
+    Time.now.strftime("%Y%m%d")
+  end
+
+protected
+
+  #
+  #   :fixd # => :fixd_root
+  def pathseg_root pathseg
+    (pathseg.to_s + '_root').to_sym
+  end
 
 end
 
