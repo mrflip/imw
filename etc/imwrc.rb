6829d34b996d4e7752aec1bc0e9748eab024003c
Dhruv Bansal <dhruv@ph.utexas.edu>
2008-09-06 02:23:00 -0500

    Fixed imwrc/imwrc.rb...again?

--- etc/imwrc.rb
+++ etc/imwrc.rb
@@ -22,14 +22,13 @@
 # License::   GPL 3.0
 # Website::   http://infinitemonkeywrench.org/
 # 
-
 module IMW
   @@paths = {
     :home      => ENV['HOME'],
-    :imw_root  => File.join(File.dirname(__FILE__), '..'),
+    :imw_root  => File.expand_path(File.join(File.dirname(__FILE__), '..')),
 
     # Data processing scripts
-    :scripts_root => [:home, 'imw', 'pool'],
+    :scripts_root => [:home, 'ics', 'pool'],
 
     # the imw library
     :imw_bin   => [:imw_root, 'bin'],
@@ -37,14 +36,17 @@ module IMW
     :imw_lib   => [:imw_root, 'lib'],
 
     # Data
-    :data_root => [:home,      'imw',  'data'],
+    :data_root => [:home,      'data'],
     :ripd_root => [:data_root, 'ripd'],
     :rawd_root => [:data_root, 'rawd'],
     :temp_root => [:data_root, 'temp'],
     :fixd_root => [:data_root, 'fixd'],
     :pkgd_root => [:data_root, 'pkgd'],
     :log_root  => [:data_root, 'log'],
+
   }
+  @@paths[:site_root] = [RAILS_ROOT] if defined?(RAILS_ROOT)
+
 end
 
-
+# puts "#{File.basename(__FILE__)}: You spend three hours configuring the settings on your Monkeywrench only to realize that you've missed your opportunity to put it to use." # at bottom
