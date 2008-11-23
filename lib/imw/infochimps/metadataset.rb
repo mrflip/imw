c172acd008f04c310cea5a7fff5b9498d02054d1
Philip (flip) Kromer <flip@infochimps.org>
2008-11-23 04:46:51 -0600

    Resolving this uri-url-link-scraped-assets everywhere business

--- lib/imw/infochimps/metadataset.rb
+++ lib/imw/infochimps/metadataset.rb
@@ -1,250 +1,133 @@
-# -*- coding: utf-8 -*-
-require 'imw/dataset/datamapper'
-require 'imw/dataset/datamapper/time_and_user_stamps'
-# Dir[File.dirname(__FILE__)+'/dataset/*'].each{|f| require f }
-require 'imw/dataset/infochimps_resource'
-require 'imw/dataset/link'
-require 'imw/dataset/asset'
-require 'imw/dataset/dataset'
-
-#
-# Datamapper interface to infochimps
-#
-
-class Contributor
+class Dataset
   include DataMapper::Resource
   include Infochimps::Resource
-  # include Sluggable
+  # Identity
   property      :id,                            Integer,        :serial      => true
   property      :name,                          String,         :length      => 255,          :nullable => false, :default => ''
   has_handle
   alias_method  :handle_generator, :name
   has_time_and_user_stamps
   #
-  property      :url,                           String,         :length      => 255,    :nullable => false, :default => ''
-  property      :desc,                          Text,                                   :nullable => false, :default => ''
-  property      :base_trustification,           Integer,                                              :default => 0
+  property      :category,                      String,         :length      =>  50,          :nullable => false, :default => ''
+  property      :collection_id,                 Integer
+  property      :is_collection,                 Boolean,        :default     => false
+  #
+  property      :valuation,                     Text,           :default     => "{}"
+  property      :metastats,                     Text,           :default     => "{}"
+  property      :facts,                         String,         :default     => "{}"
+  property      :delicious_taggings,            Integer
+  property      :base_trust,                    Integer
+  property      :trust_value,                   Integer,                                :index => :trust_value
+  property      :num_delicious_savers,          Integer,                                :index => :num_delicious_savers
+  property      :skip_me,                       Boolean
   #
   has n,        :credits
-  has n,        :datasets,    :through => :credits
-  has n,        :taggings,                            :child_key => [:tagger_id]
-  has n,        :tags,        :through => :taggings,  :child_key => [:tagger_id]
-  has n,        :taggables,   :through => :taggings,  :child_key => [:tagger_id], :class_name => 'Dataset'
+  has n,        :contributors, :through     => :credits
+  has n,        :notes,                                     :child_key   => [:noteable_id]
+  has n,        :payloads
+  has n,        :ratings,                                   :child_key   => [:rateable_id]
+  has 1,        :license_info
+  has 1,        :license,     :through     => :license_info
+  has n,        :taggings,                                  :child_key => [:taggable_id]
+  has n,        :tags,        :through => :taggings,        :child_key => [:taggable_id]
+  has n,        :taggers,     :through => :taggings,        :child_key => [:taggable_id], :class_name => 'Contributor'
+  has n,        :linkings,                                  :child_key => [:linkable_id]
+  has n,        :links,       :through => :linkings,        :child_key => [:linkable_id]
+
+  #===============================================================================
   #
   # Macros
   #
-  # self.slug_on :url
-end
+  # slug_on       :name
+  # before :save, :force_approval
+  # before :save, :insert_default_rights_statement
+  # before :save, :insert_default_link
 
-class Credit
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial      => true
-  has_time_and_user_stamps
-  has_handle
-  def handle_generator()  [dataset_id, contributor_id, role].join '-' end
+  #===============================================================================
   #
-  property      :dataset_id,                    Integer
-  property      :contributor_id,                Integer
+  # Methods
   #
-  property      :role,                          String,         :length      =>  40,    :nullable => false, :default => ''
-  property      :desc,                          Text,                                   :nullable => false, :default => ''
-  property      :citation,                      Text,                                   :nullable => false, :default => ''
+  def description
+    @description ||= self.notes.first({ :role => 'description' })
+  end
+  def set_note(role, text, name)
+    a_note = self.notes.find_or_create({ :role => role, :noteable_id => self.id })
+    self.notes << a_note
+    a_note.name = name
+    a_note.desc = text
+    a_note.save
+    a_note
+  end
+  def description=(text)
+    @description = self.notes.find_or_create({ :role => 'description', :noteable_id => self.id })
+    self.notes << @description
+    @description.desc = text
+    @description.save
+  end
+  # tags are ',' separated
+  def tag_with(context, tags_list)
+    return if tags_list.blank?
+    tag_strs = tags_list.split(',').map{|s| s.gsub(/[^\w]+/,'') }.reject(&:blank?)
+    tag_strs.each do |tag_str|
+      tag     = Tag.find_or_create({ :name => tag_str })
+      tagging = Tagging.find_or_create({ :tag_id => tag.id, :context => context, :taggable_id => self.id, :taggable_type => self.class.to_s })
+    end if tag_strs
+  end
+  # adds a note with _context (hiding it from normal view)
   #
-  belongs_to    :dataset
-  belongs_to    :contributor
-end
+  def add_internal_note context, info
+    note = internal_note context
+    note.desc = info.to_yaml
+    note.save
+  end
+  def internal_note context
+    note = self.notes.find_or_create( :role => "_#{context}" )
+    self.notes << note
+    note
+  end
+  def register_info key, val
+    note = internal_note(:info)
+    info = YAML.load(note.desc) || {}
+    (info[key]||=[]) << val
+    info[key].uniq!
+    note.desc = info.to_yaml
+    note.save
+    note
+  end
+  def credit contributor, attrs
+    self.credits << self.credits.find_or_create({ :contributor_id => contributor.id, }, attrs)
+  end
 
-class Linking
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial      => true
-  has_handle
-  def handle_generator()  [linkable_type, linkable_id, link_id, role].join '-' end
-  has_time_and_user_stamps
-  #
-  property      :linkable_id,                   Integer,                                                                        :index => :linkable_index
-  property      :link_id,                       Integer,                                                                        :index => :linkable_index
-  property      :role,                          String,         :length      =>  40,    :nullable => false, :default => '',     :index => :linkable_index
-  property      :linkable_type,                 String,         :length      =>  40,    :nullable => false,                     :index => :linkable_index
-  #
-  belongs_to    :linkable, :class_name => 'Dataset', :child_key => [:linkable_id],       :polymorphic  => true
-  belongs_to    :link
-end
+  def force_approval
+    [:approved_by, :created_by, :updated_by,].each do |actor|
+      self.send("#{actor}=", User.find_by_login('flip').id)
+    end
+    self.approved_at ||= Time.now
+  end
 
-class Tagging
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial      => true
-  has_handle
-  def handle_generator()  [taggable_type, taggable_id, tagger_type, tagger_id, tag_id].join '-' end
-  has_time_and_user_stamps
-  #
-  property      :context,                       String,         :length      =>  40,    :nullable => false, :default => ''
-  property      :tag_id,                        Integer
-  property      :taggable_id,                   Integer
-  property      :taggable_type,                 String,         :length      =>  40,    :nullable => false
-  #
-  property      :tagger_id,                     Integer
-  property      :tagger_type,                   String
-  #
-  belongs_to    :tagger,   :class_name => 'Contributor', :child_key => [:tagger_id],        :polymorphic  => true
-  belongs_to    :taggable, :class_name => 'Dataset',     :child_key => [:taggable_id],      :polymorphic  => true
-  belongs_to    :tag
-  before :save, :fake_tagger_polymorphism; def fake_tagger_polymorphism() self.tagger_type ||= 'Contributor' end
-end
+  def insert_default_rights_statement
+    if !self.rights_statement
+      self.rights_statement = RightsStatement.create(:license => License.find_by_handle(:needs_rights))
+    end
+  end
 
-class Tag
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial      => true
-  property      :name,                          String,         :length      => 255,    :nullable => false, :default => ''
-  has_handle                                                    :length      => 255
-  alias_method  :handle_generator, :name
-  #
-  has n,        :taggings
-  has n,        :taggables, :through => :taggings
-  has n,        :taggers,   :through => :taggings
-end
+  def insert_default_link
+    if links.empty?
+      l = links.find_or_create({:role => :main}, :full_url => url, :name => description.desc)
+      links << l
+    end
+  end
 
-class Note
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial => true
-  property      :name,                          String,         :length      => 255,    :nullable => false, :default => ''
-  has_handle
-  def handle_generator()  [noteable_type, noteable_id, role].join '-' end
-  has_time_and_user_stamps
-  #
-  property      :noteable_id,                   Integer
-  property      :noteable_type,                 String,         :length      =>  40,    :nullable => false
-  property      :role,                          String,         :length      =>  40,    :nullable => false, :default => ''
-  property      :desc,                          Text,                                   :nullable => false, :default => ''
-  #
-  belongs_to    :noteable, :class_name => 'Dataset', :child_key => [:noteable_id],       :polymorphic  => true
-  before :save, :fake_polymorphism; def fake_polymorphism() self.noteable_type = 'Dataset' end
-end
+  attr_accessor :fact_hash
+  def fact_hash
+    @fact_hash ||= self.facts.blank? ? { } : YAML.load(self.facts)
+  end
+  before :save, :serialize_facts
+  def serialize_facts
+    self.facts = @fact_hash.to_yaml if @fact_hash
+  end
 
-class Rating
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial => true
-  has_handle
-  def handle_generator()  [rateable_type, rateable_id, context].join '-' end
-  has_time_and_user_stamps
-  #
-  property      :user_id,                       Integer
-  property      :rateable_id,                   Integer
-  property      :rateable_type,                 String,         :length      =>  40,    :nullable => false
-  #
-  property      :rating,                        Integer,                                                    :default => 0
-  property      :context,                       String,         :length      =>  40,    :nullable => false, :default => "overall"
-  #
-  belongs_to    :rateable, :class_name => 'Dataset', :child_key => [:rateable_id],      :polymorphic  => true
-  belongs_to    :user
-  before :save, :fake_polymorphism; def fake_polymorphism() self.rateable_type = 'Dataset' end
-end
-
-class License
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial => true
-  property      :name,                          String,         :length      => 255,    :nullable => false, :default => ''
-  has_handle
-  alias_method  :handle_generator, :name
-  has_time_and_user_stamps
-  #
-  property      :url,                           String,         :length      => 255,    :nullable => false, :default => ''
-  property      :desc,                          Text,                                   :nullable => false, :default => ''
-  #
-  has n,        :license_infos
-  has n,        :datasets,      :through => :license_infos
-end
-
-class LicenseInfo
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial => true
-  has_handle
-  def handle_generator()  [dataset_id, license_id].join '-' end
-  has_time_and_user_stamps
-  #
-  property      :dataset_id,                    Integer
-  property      :license_id,                    Integer
-  #
-  property      :url,                           String,         :length      => 255,    :nullable => false, :default => ''
-  property      :desc,                          Text,                                   :nullable => false, :default => ''
-  #
-  belongs_to    :license
-  belongs_to    :dataset
-end
-
-class Payload
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial => true
-  property      :file_name,                     String,         :length      => 150,    :nullable => false, :default => ''
-  property      :file_path,                     String,         :length      => 2048,   :nullable => false, :default => ''
-  has_handle
-  alias_method  :handle_generator, :file_path
-  has_time_and_user_stamps
-  #
-  property      :dataset_id,                    Integer
-  #
-  property      :file_date,                     DateTime
-  property      :format,                        String,         :length      => 40,     :nullable => false, :default => ''
-  property      :shape,                         String
-  property      :size,                          Integer
-  property      :stats,                         Text
-  #
-  property      :signature,                     Text
-  property      :signed_by,                     Integer
-  property      :fingerprint,                   String,         :length      => 40,     :nullable => false, :default => ''
-  #
-  belongs_to    :dataset
-end
-
-class Field
-  include DataMapper::Resource
-  include Infochimps::Resource
-  property      :id,                            Integer,        :serial => true
-  has_handle
-  def handle_generator()  [dataset_id, name].join '-' end
-  has_time_and_user_stamps
-  #
-  property      :dataset_id,                    Integer
-  property      :table_id,                      Integer
-  #
-  property      :name,                          String,         :length      => 150,    :nullable => false, :default => ''
-  property      :desc,                          Text,                                   :nullable => false, :default => ''
-  property      :datatype,                      String,         :length      =>  40,    :nullable => false, :default => ''
-  property      :representation,                String,         :length      => 255,    :nullable => false, :default => ''
-  property      :concepts,                      String,         :length      => 255,    :nullable => false, :default => ''
-  property      :constraints,                   String,         :length      => 255,    :nullable => false, :default => ''
-  property      :stats,                         Text,                                   :nullable => false, :default => ''
-  belongs_to    :dataset
-end
-
-class InfochimpsUser
-  include DataMapper::Resource
-  include Infochimps::Resource
-  include DataMapper::Timestamp
-  property      :id,                            Integer,        :serial  => true
-  property      :login,                         String,         :length  =>  40,        :nullable => false
-  has_handle
-  alias_method  :handle_generator, :login
-  #
-  property      :prefs,                         Text
-  #
-  property      :identity_url,                  String,         :length  => 255,        :nullable => false, :unique      => true
-  property      :name,                          String,         :length  => 100,        :nullable => false
-  property      :email,                         String,         :length  => 100,        :nullable => false
-  property      :email_is_public,               Boolean,                                              :default => false
-  property      :homepage_link,                 String,         :length  => 255,        :nullable => false, :default     => ''
-  property      :blurb,                         Text,           :length  =>2048,        :nullable => false, :default     => ''
-  #
-  property      :public_key,                    Text
-  property      :email_verification_code,       String,         :length  => 40
-  property      :email_verified_at,             DateTime
-  property      :roles,                         String,         :length  => 2048
+  def set_fact attr, key, val
+    self.fact_hash[key] = val
+  end
 end
