e5ffadd50786536203cc89a7f2ea383d28ed5016
Dhruv Bansal <dhruv@ph.utexas.edu>
2009-01-02 20:43:24 -0600

    Added tasks.rb and dependencies.rb to manage default tasks and dependencies for dataset workflows.

---
+++ lib/imw/workflow/dependencies.rb
@@ -0,0 +1,17 @@
+#
+# h2. lib/imw/workflow/dependencies.rb -- dependencies between workflow tasks
+#
+# == About
+#
+# Defines a few default dependencies between default tasks (as defined
+# in <tt>imw/workflow/tasks.rb</tt>).
+#
+# Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
+# Copyright:: Copyright (c) 2008 infochimps.org
+# License::   GPL 3.0
+# Website::   http://infinitemonkeywrench.org/
+#
+# puts "#{File.basename(__FILE__)}: Something clever" # at bottom
+
+
+require 'imw/utils'
---
+++ lib/imw/workflow/tasks.rb
@@ -0,0 +1,17 @@
+#
+# h2. lib/imw/workflow/tasks.rb -- default tasks for workflows
+#
+# == About
+#
+# This file defines tasks that dataset workflows have in common.
+#
+# Author::    (Philip flip Kromer, Dhruv Bansal) for Infinite Monkeywrench Project (mailto:coders@infochimps.org)
+# Copyright:: Copyright (c) 2008 infochimps.org
+# License::   GPL 3.0
+# Website::   http://infinitemonkeywrench.org/
+#
+# puts "#{File.basename(__FILE__)}: Something clever" # at bottom
+
+require 'imw/utils'
+
+
