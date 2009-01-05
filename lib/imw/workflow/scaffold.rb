b1e203703c3e3d88da34a092e90657d2537243ec
Dhruv Bansal <dhruv@ph.utexas.edu>
2009-01-05 00:42:12 -0600

    Have default tasks to create directories, symlinks, and to delete data for a dataset.

--- lib/imw/workflow/scaffold.rb
+++ lib/imw/workflow/scaffold.rb
@@ -82,7 +82,7 @@ module IMW
       @last_description = "Creates directories for this dataset in the peel through package steps."
       define_task(Rake::Task, {:create_directories => []}) do
         [:peel, :munge, :fix, :package].each do |step|
-          puts "I am going to create a path to #{path_to step}"
+          FileUtils.mkdir_p(path_to(step)) unless File.exist?(path_to(step))
         end
       end
     end
@@ -91,9 +91,26 @@ module IMW
     # the directory structure for this dataset.
     def create_symlinks_task
       @last_description = "Creates symlinks pointing from the directory containing scripts for this dataset to the directories for the peel through package steps."
-      define_task(Rake::Task, {:create_symlinks => [:create_directories]})
+      define_task(Rake::Task, {:create_symlinks => [:create_directories]}) do
+        [:peel, :munge, :fix, :package].each do |step|
+          symlink = File.join(path_to(:script),IMW::Dataset::WORKFLOW_STEP_DIRS[step].to_s)
+          FileUtils.ln_s(path_to(step), symlink) unless File.exist?(symlink)
+        end
+        symlink = File.join(path_to(:script), "ripd")
+        FileUtils.ln_s(path_to(:ripd_root), symlink) unless File.exist?(symlink)
+      end
     end
 
+    # Removes all data for this dataset from the data directories.
+    def create_delete_data_task
+      @last_description = "Deletes all data and directories for this dataset for the peel through package steps."
+      define_task(Rake::Task, {:delete_data => []}) do
+        [:peel, :munge, :fix, :package].each do |step|
+          FileUtils.remove_dir(path_to(step)) if File.exist?(path_to(step))
+        end
+      end
+    end
+    
   end
 end
 
