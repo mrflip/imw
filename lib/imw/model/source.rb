6d8f2dc163f6ece7b285215f015d3a5636e7d1aa
Dhruv Bansal <dhruv@ph.utexas.edu>
2009-01-05 12:00:41 -0600

    Working on including a read/write mode argument for IMW.open.

--- lib/imw/model/source.rb
+++ lib/imw/model/source.rb
@@ -19,19 +19,20 @@ require 'uri'
 
 module IMW
 
-  # Parse +source+ and return an appropriate data object.
+  # Parse +source+ and return an appropriate data object.  Mode can be
+  # either <tt>'r'</tt> for read (default) or <tt>'w'</tt> for write.
   #
   #   IMW.open("/tmp/test.csv") # => IMW::Files::Csv("/tmp/test.csv')
   #
   # The objects returned by <tt>IMW.open</tt> present a uniform
   # interface across the different source data formats they handle.
-  def self.open source, options = {}
+  def self.open source, mode='r', options = {}
     uri = URI.parse(source)
     method = {
       "file" => :open_file,
       "http" => :open_http
     }.dispatch(:open_file) {|scheme| uri.scheme == scheme}
-    IMW::Source.send(method,uri,options)
+    IMW::Source.send(method,uri,mode,options)
   end
   
   # The <tt>IMW::Source</tt> module contains functions which wrap the
@@ -40,12 +41,12 @@ module IMW
 
     protected
     # Open a file at the given +uri+.
-    def self.open_file uri, options = {}
+    def self.open_file uri, mode='r', options = {}
       require 'imw/model/files'
       class_name = IMW::Files::FILE_REGEXPS.dispatch("Text") {|regexp| regexp.match(uri.path)}
       # FIXME this use of 'eval' can't be the right way to do what i
       # want?
-      eval("IMW::Files::#{class_name}.new(\"#{uri.path}\",options)")
+      eval("IMW::Files::#{class_name}.new(\"#{uri.path}\",mode,options)")
     end
   end
 end
