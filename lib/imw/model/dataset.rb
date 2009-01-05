82e4bb73117513658736c27f05e46482644be9b7
Dhruv Bansal <dhruv@ph.utexas.edu>
2009-01-04 23:30:44 -0600

    Got rid of the IMW::Workflow class and moved the functionality of it directly to IMW::Dataset, which feels more natural.  Also, defining workflow tasks is easier because everything has access to the path_to method of the IMW::Dataset.  Set it up so that I can easily define and invoke tasks.  This part works!

--- lib/imw/model/dataset.rb
+++ lib/imw/model/dataset.rb
@@ -9,6 +9,9 @@
 # Website::   http://infinitemonkeywrench.org/
 #
 
+require 'rake'
+require 'ostruct'
+
 require 'imw/utils'
 require 'imw/workflow'
 
@@ -23,22 +26,89 @@ module IMW
   # <tt>IMW::Dataset::DEFAULT_TAXON</tt>) which determines where files
   # are kept during processing.
   #
-  # Each dataset also has a +workflow+ which leverages the
-  # functionality of Rake[http://rake.rubyforge.org/] to manage tasks
-  # associated with the processing of the dataset along with their
-  # dependencies.
+  # Each dataset is also capable of managing a collection of tasks
+  # using the Ruby Rake[http://rake.rubyforge.org/] library.
   class Dataset
+
+    # The <tt>Rake::TaskManager</tt> module allows the
+    # <tt>IMW::Dataset</tt> class to leverage the functionality of the
+    # Rake[http://rake.rubyforge.org/] library to manage tasks
+    # associated with the processing of this dataset.
+    include Rake::TaskManager
+
+    # The <tt>IMW::Workflow</tt> module contains pre-defined tasks for
+    # dataset processing.
+    include IMW::Workflow
+
+    attr_reader :handle, :taxon, :options
 
     # The default taxon assigned to a dataset.
     DEFAULT_TAXON = ["misc"]
 
-    attr_reader :handle, :workflow, :taxon
+    # The steps in the IMW workflow, in order from first to last.
+    # 
+    #   <tt>:rip</tt>:: Data is collected from some source and
+    #   deposited in the <tt>:ripd</tt> directory named by the URI for
+    #   that source.
+    #   
+    #   <tt>:peel</tt>:: Data is uncompressed and extracted (if
+    #   necessary) from source(s') <tt>:ripd</tt> directory(ies) and
+    #   unprocessed files are placed in this dataset's <tt>:peeld</tt>
+    #   directory.
+    #
+    #   <tt>:munge</tt>:: Data is read from the <tt>:peeld</tt>
+    #   directory, transformed, and placed into the <tt>:mungd</tt>
+    #   directory.
+    #
+    #   <tt>:fix</tt>:: Global quantities (i.e. - averages) are
+    #   calculated and records are reconciled and written to the
+    #   <tt>:fixd</tt> directory.
+    #
+    #   <tt>:package</tt>:: Data is packaged and compressed (if
+    #   necessary) into a delivery format and deposited into the
+    #   <tt>:pkgd</tt> directory</tt>.
+    #
+    # I think better names might be <tt>:harvest</tt>, <tt>:peel</tt>,
+    # <tt>:chew</tt>, <tt>:digest</tt>, and <tt>:shit_out</tt>.  Well,
+    # maybe we should stick to <tt>:package</tt>, but still...
+    WORKFLOW_STEPS = [:rip, :peel, :munge, :fix, :package]
+
+    # Hash to convert between the name of a workflow step and the
+    # directory which stores data for that step.  Not every step in
+    # the workflow has a corresponding data directory.
+    WORKFLOW_STEP_ROOTS = {
+      :rip     => :ripd_root,      
+      :peel    => :peeld_root,
+      :munge   => :mungd_root,
+      :fix     => :fixd_root,
+      :package => :pkgd_root
+    }
+    
+    # Default options passed to <tt>Rake</tt>.  Any class including
+    # the <tt>Rake::TaskManager</tt> module must define a constant by
+    # this name.
+    DEFAULT_OPTIONS = {
+      :dry_run => false,
+      :trace => false,
+      :verbose => false
+    }
 
     # Create a new dataset with the given +handle+.
     def initialize handle
-      @handle = handle.to_sym
-      @workflow = IMW::Workflow.new
+      @handle = handle
       @taxon = DEFAULT_TAXON
+
+      # for rake
+      @tasks = Hash.new
+      @rules = Array.new
+      @scope = Array.new
+      @last_description = nil
+      @options = OpenStruct.new(DEFAULT_OPTIONS)
+      create_default_tasks
+    end
+
+    def handle= thing
+      thing.is_a?(String) ? thing.to_handle : thing
     end
 
     # If the +taxon+ given is a sequence then it is directly
@@ -63,19 +133,13 @@ module IMW
       end
     end
 
-    # If +place+ is one of the steps in
-    # <tt>IMW::Workflow::STEP_ROOTS</tt> then return the directory
-    # corresponding to this dataset's files, as determined by the step
-    # and this dataset's taxon, otherwise just return whatever
-    # <tt>IMW.path_to</tt> would return.
+    # If +place+ is one of the steps in +WORKFLOW_STEP_ROOTS+ then
+    # return the directory corresponding to this dataset's files, as
+    # determined by the step and this dataset's taxon, otherwise just
+    # return whatever <tt>IMW.path_to</tt> would return.
     def path_to place
-      if IMW::Workflow::STEP_ROOTS.key?(place) then
-        IMW.path_to IMW::Workflow::STEP_ROOTS[place], @taxon
-      else
-        IMW.path_to place
-      end
+      WORKFLOW_STEP_ROOTS.key?(place) ? IMW.path_to(WORKFLOW_STEP_ROOTS[place], @taxon) : IMW.path_to(place)
     end
-
   end
 end
 
--- lib/imw/workflow.rb
+++ lib/imw/workflow.rb
@@ -12,106 +12,20 @@
 # Website::   http://infinitemonkeywrench.org/
 # 
 
-require 'rake'
-require 'ostruct'
-
-require 'imw/utils'
+require 'imw/workflow/scaffold'
 
 module IMW
 
-  # <tt>IMW::Workflow</tt> is a class for managing the collection of
-  # interdependent tasks involved in processing a dataset.  It uses
-  # <tt>Rake</tt> (the <tt>Rake::TaskManager</tt> class, specifically)
-  # to handle dependency management.
-  class Workflow
+  # The <tt>IMW::Workflow</tt> module is a collection of methods which
+  # define Rake[http://rake.rubyforge.org/] tasks specialized for each
+  # dataset.
+  module Workflow
 
-    include Rake::TaskManager
-
-    # The steps in the IMW workflow, in order from first to last.
-    #   <tt>:rip</tt>:: Data is collected from some source and
-    #   deposited in the <tt>:ripd</tt> directory named by the URI for
-    #   that source.
-    #   
-    #   <tt>:peel</tt>:: Data is uncompressed and extracted (if
-    #   necessary) from source(s') <tt>:ripd</tt> directory(ies) and
-    #   unprocessed files are placed in this dataset's <tt>:rawd</tt>
-    #   directory.
-    #
-    #   <tt>:munge</tt>:: Data is transformed.
-    #
-    #   <tt>:fix</tt>:: Global quantities (i.e. - averages) are
-    #   calculated and records are reconciled.
-    #
-    #   <tt>:package</tt>:: Data is packaged and compressed (if
-    #   necessary) into a delivery format.
-    #
-    # I think better names might be <tt>:harvest</tt>, <tt>:peel</tt>,
-    # <tt>:chew</tt>, <tt>:digest</tt>, and <tt>:shit_out</tt>.  Well,
-    # maybe we should stick to <tt>:package</tt>, but still...
-    STEPS = [:rip, :peel, :munge, :fix, :package]
-
-    # Hash to convert between the name of a workflow step and the
-    # directory which stores data for that step.  Not every step in
-    # the workflow has a corresponding data directory.
-    STEP_ROOTS = {
-      :rip => :ripd_root,
-      :peel => :peeld_root,
-      :fix => :fixd_root,
-      :package =>:pkgd_root
-    }
-
-    attr_reader :options
-
-    # Default options passed to <tt>Rake</tt>.  Any class including
-    # the <tt>Rake::TaskManager</tt> module must define a constant by
-    # this name.
-    DEFAULT_OPTIONS = {
-      :dry_run => false,
-      :trace => false,
-      :verbose => false
-    }
-
-    def initialize
-      super
-      # The <tt>Rake::TaskManager</tt> class requires an including class
-      # <tt>Example</tt> to define an <tt>OpenStruct</tt> object
-      # <tt>Example.options</tt> which individual <tt>Rake::Task</tt>'s
-      # check before performing their actions.
-      @options = OpenStruct.new(DEFAULT_OPTIONS)
-      set_default_tasks
+    def create_default_tasks
+      create_directories_task
+      create_symlinks_task
     end
-
-    # Sets the default tasks for this workflow.
-    #
-    # The  of actions that depend upon
-    # one another in a consecutive way (see
-    # <tt>IMW::Workflow::STEPS</tt>).  Each task is a
-    # <tt>Rake::Task</tt> which depends on the one before it.
-    # 
-    # Each task does nothing by default other than create directories
-    # to hold files for this dataset as it undergoes the workflow.
-    def set_default_tasks
-      define_task(Rake::Task, {:rip => []})
-      define_task(Rake::Task, {:peel => :rip})
-      define_task(Rake::Task, {:munge => :peel})
-      define_task(Rake::Task, {:fix => :munge})
-      define_task(Rake::Task, {:package => :fix})
-
-      comment_default_tasks
-
-    end
-
-    # Set the initial comments for each of the default tasks.
-    def comment_default_tasks
-      self[:rip].comment = "Obtain a dataset from an origin"
-      self[:peel].comment = "Extract a dataset and prepare it for processing."
-      self[:munge].comment = "Munge dataset's records into desired form"
-      self[:fix].comment = "Reconcile records in a dataset"
-      self[:package].comment = "Package dataset into a final format"
-    end
-    
   end
 end
-
 
 # puts "#{File.basename(__FILE__)}: You find your flow next to a tall tree.  Ahhhh."
