37571a71eb751698e3ec43fc8c7e17d9be9aaf3b
Dhruv Bansal <dhruv@ph.utexas.edu>
2009-01-23 00:48:28 -0600

    IMW::Files::Xml and IMW::Files::Html now both subclass IMW::Files::SGML so no need to duplicate common methods.

--- lib/imw/files/sgml.rb
+++ lib/imw/files/sgml.rb
@@ -21,7 +21,7 @@ require 'imw/files/compressible'
 module IMW
   module Files
 
-    class Xml < Hpricot::Doc
+    class Sgml < Hpricot::Doc
 
       include IMW::Files::BasicFile
       include IMW::Files::Compressible
@@ -33,20 +33,12 @@ module IMW
 
     end
 
+    class Xml < Sgml
+    end
     FILE_REGEXPS[Regexp.new("\.xml$")] = IMW::Files::Xml
 
-    class Html < Hpricot::Doc
-
-      include IMW::Files::BasicFile
-      include IMW::Files::Compressible
-
-      def initialize path,mode='r',options = {}
-        self.path= path
-        super Hpricot.make(File.new(@path).read),options
-      end
-
+    class Html < Sgml
     end
-
     FILE_REGEXPS[Regexp.new("\.html$")] = IMW::Files::Html
     FILE_REGEXPS[Regexp.new("\.htm$")]  = IMW::Files::Html
   end
