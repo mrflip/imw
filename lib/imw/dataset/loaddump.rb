003db431a6b8ec29bae4759d560e3e5bc4113c91
Dhruv Bansal <dhruv@ph.utexas.edu>
2009-01-23 00:46:35 -0600

    Got loaddump.rb using IMW.open.

--- lib/imw/dataset/loaddump.rb
+++ lib/imw/dataset/loaddump.rb
@@ -18,45 +18,31 @@ require 'imw/utils'
 module IMW
   class Dataset
 
-    # Guess the file format of a given file.
-    def self.file_format filename
-      File.extname(path_to(filename)).gsub(/^\./,'').to_sym
-    end
-
+    # Return the data in +filename+ in an appropriate form.
+    #
+    # FIXME How do I get pass a block from one method to another?
     def self.load filename, &block
-      filename = path_to(filename)
+      filename = path_to(filename)      
       announce "Loading #{filename}"
-      format ||= file_format(filename)
-      file     = File.open(filename)
-      data = case format
-      when :yaml
-        YAML.load file
+      file = IMW.open(filename)
+      data = file.load(filename)
+      if block
+        data.each{|record| yield record}
+        file
       else
-        raise "Don't know how to dump a #{format} file"
+        data
       end
-      data = yield data if block
-      self.new(data)
     end
 
-    #
-    # Dump the given data structure to a file,
-    # guessing the format and all that stupid crap.
-    #
-    def self.dump data, filename, format = nil
+    # Dump +data+ to +filename+.
+    def self.dump data, filename
       filename = path_to(filename)
-      announce "Dumping #{filename}"
-      format ||= Dataset.file_format(filename)
-      file     = File.open(filename, 'w')
-      case format
-      when :yaml
-        YAML.dump(data, file)
-      else
-        raise "Don't know how to dump a #{format} file"
-      end
+      announce "Dumping to #{filename}"
+      IMW.open(filename,'w').dump(data)
     end
-    #
-    # dispatch to Dataset.dump
-    def dump *args
+
+    # Dispatch to <tt>Dataset.dump</tt>.
+    def dump filename
       self.class.dump self.data, *args
     end
 
