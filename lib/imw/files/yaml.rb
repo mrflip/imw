67810e31c8dc67bbe93e5dc6cfd931c7830ce6ab
Dhruv Bansal <dhruv@ph.utexas.edu>
2009-01-23 00:49:07 -0600

    Added load/dump methods for IMW::Files::Yaml.

--- lib/imw/files/yaml.rb
+++ lib/imw/files/yaml.rb
@@ -21,19 +21,22 @@ module IMW
 
     class Yaml < IMW::Files::Text
 
-      attr_reader :contents
-
       def initialize path, mode='r', options = {}
         super path, mode
-        unless mode == 'w' then
-          @contents = YAML.load_file @path
-        end
       end
 
-      # FIXME should a `<<' method be implemented which forces
-      # non-literal content to be converted to yaml before being
-      # written to file?
+      # Return the contents of this YAML file.
+      #
+      # FIXME what to do if a block is passed in?
+      def load &block
+        YAML.load_file @path
+      end
 
+      # Dump +data+ to this file as YAML.
+      def dump data
+        super data.to_yaml
+      end
+      
     end
 
     FILE_REGEXPS[Regexp.new("\.yaml$")] = IMW::Files::Yaml
