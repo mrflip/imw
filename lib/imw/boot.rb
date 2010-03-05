0621a59fcae2eb34296807f24b9e8a3faa3fd800
Dhruv Bansal <dhruv@ph.utexas.edu>
2010-03-05 16:08:35 -0600

    Added ability to have /etc/imwrc.rb override default settings.  ~/.imwrc still overrides this.  and IMWRC env variable overrides everything.

--- lib/imw/boot.rb
+++ lib/imw/boot.rb
@@ -11,11 +11,12 @@ module IMW
     #
     # User configuration file
     #
-    # By default, the file ~/.imwrc (.imwrc, in your home directory -- note no .rb extension)
-    # is sourced at top level.  If the $IMWRC environment variable is set,
-    # that file will be sourced instead.
+    # By default, the file ~/.imwrc (.imwrc, in your home directory --
+    # note no .rb extension) is sourced at top level.  If the $IMWRC
+    # environment variable is set, that file will be sourced instead.
     #
-    # Any code within this file will override settings in IMW_ROOT/etc/imwrc.rb
+    # Any code within this file will override settings in
+    # /etc/imwrc.rb which itself overrides IMW_ROOT/etc/imwrc.rb
     #
     USER_CONFIG_FILE = File.join(ENV['HOME'] || '', '.imwrc')
     # Environment variable to override user configuration file location.
@@ -24,16 +25,31 @@ module IMW
       File.expand_path(ENV[ENV_CONFIG_FILE] || USER_CONFIG_FILE)
     end
 
-    # System-level config file
-    SITE_CONFIG_FILE = "etc/imwrc.rb"
+    # Path to site-wide config file (overwrites IMW defaults but
+    # overridden by user defaults).
+    SITE_CONFIG_FILE = "/etc/imwrc.rb"
     def self.site_config_file # :nodoc:
-      File.join(imw_root, SITE_CONFIG_FILE)
+      SITE_CONFIG_FILE
     end
 
+    def self.default_config_file # :nodoc:
+      File.join(imw_root, "etc/imwrc.rb")
+    end
+    
     # Source the config files
     def self.load_config
-      load user_config_file if File.exist? user_config_file
-      require site_config_file
+      if File.exist?(user_config_file)
+        puts "loading user config file (#{user_config_file})"
+        load user_config_file
+      end
+
+      if File.exist?(site_config_file)
+        puts "requiring site config file (#{site_config_file})"
+        load site_config_file
+      end
+
+      load default_config_file
+        
     end
   end
 end
